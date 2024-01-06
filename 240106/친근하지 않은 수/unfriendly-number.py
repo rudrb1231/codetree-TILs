c1121aa3f5f015b6b855b5ac80aae53ac0e6d503
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-06 16:22:16 +0900

    Title: 친근하지 않은 수, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240106/친근하지 않은 수/unfriendly-number.py
+++ 240106/친근하지 않은 수/unfriendly-number.py
@@ -1,7 +1,9 @@
 inp=int(input())
 n=0
 
-for i in range(1,inp):
-    if i%2==0 and i%3==0 or i%5==0:
-        n+=1
+for i in range(1,inp+1):
+    if i%2==0 or i%3==0 or i%5==0:
+        continue
+    n+=1
+   
 print(n)
