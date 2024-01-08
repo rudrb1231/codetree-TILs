54ca7751757654cb10422a3ca8d2420e7a7d1cce
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-09 00:07:16 +0900

    Title: 공약수의 유무, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240109/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,12 @@
+n=input()
+arr=n.split()
+a,b=int(arr[0]),int(arr[1])
+sat=False
+for i in range(a,b):
+    if 1920%i==0 and 2880%i==0:
+        sat=True
+
+if sat==True:
+    print('1')
+else:
+    print('0')
