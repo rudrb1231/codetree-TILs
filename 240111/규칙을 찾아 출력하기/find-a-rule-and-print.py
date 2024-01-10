5430abe2797c6276ccb23520ee8e5f7a799ebb1c
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-11 03:06:08 +0900

    Title: 규칙을 찾아 출력하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240111/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1:
+            print('*',end=' ')
+        else:
+            if i> j or j==n-1:
+                print('*',end=' ')
+            else:
+                print(' ',end=' ')
+    print()
