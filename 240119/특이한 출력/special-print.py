aaff330b8fd9b0aec03626cf5ac3d9283e912816
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-19 21:27:16 +0900

    Title: 특이한 출력, Time: 207ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/특이한 출력/special-print.py
@@ -0,0 +1,9 @@
+n=int(input())
+
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i+j==4:
+            print(f"({i}, {j})",end='\n')
+        else:
+            print(f"({i}, {j})",end=' ')
