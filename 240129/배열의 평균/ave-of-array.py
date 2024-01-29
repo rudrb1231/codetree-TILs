a9fa8c64f2f2317c9312d8a62a908c48c0b9e6fc
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-29 22:21:18 +0900

    Title: 배열의 평균, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/배열의 평균/ave-of-array.py
+++ 240129/배열의 평균/ave-of-array.py
@@ -7,5 +7,17 @@ for i in range(2):
     v=0
     for j in range(4):
         v+=arr[i][j]
-        print(f"{v:.1f}",end=' ')
-    print()
+    print(f"{v/4:.1f}",end=' ')
+print()
+
+for j in range(4):
+    v=0
+    for i in range(2):
+        v+=arr[i][j]
+    print(f"{v/2:.1f}",end=' ')
+print()
+v=0
+for i in range(2):
+    for j in range(4):
+        v+=arr[i][j]
+print(f"{v/8:.1f}")
