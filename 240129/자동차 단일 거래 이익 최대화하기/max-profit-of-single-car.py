f1420628ca92b34e8ceba449333a3e11f126ffae
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-29 21:13:43 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240129/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240129/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,24 +1,15 @@
 n=int(input())
 
-arr=list(map(int,input().split()))
+price=list(map(int,input().split()))
 
 
-max_ben=0
-min_buy=100
-ben=[]
+max_profit=0
 for i in range(n):
-    if n==1:
-        break
+    for j in range(i+1,n):
+        profit = price[j]-price[i]
 
-    if arr[i]<min_buy:
-        min_buy=arr[i]
-    
-    if min_buy<arr[i]:
-        max_ben=arr[i]-min_buy
-        ben.append(max_ben)
+        if profit > max_profit:
+            max_profit = profit
 
-for elem in ben:
-    if elem>max_ben:
-        max_ben=elem
 
-print(max_ben)
+print(max_profit)
