1f4ce89bfae62d82522113964131ed454cd1d01e
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-31 23:08:55 +0900

    Title: 가장 짧은 문자열, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/가장 짧은 문자열/shortest-string.py
+++ 240131/가장 짧은 문자열/shortest-string.py
@@ -1,12 +1,8 @@
-a=input()
-b=input()
-c=input()
+a=len(input())
+b=len(input())
+c=len(input())
 
-if len(c)>len(b) and len(b)>len(a):
-    print(len(c)-len(a))
-elif len(b)>len(a) and len(a)>len(c):
-    print(len(b)-len(c))
-elif len(a)>len(b) and len(b)>len(c):
-    print(len(a)>len(c))
-else:
-    print(len(b)-len(a))
+d=max(a,b,c)
+e=min(a,b,c)
+
+print(d-e)
