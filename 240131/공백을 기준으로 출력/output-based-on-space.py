787070bd04ea2126c28e0fa7312334337dacce40
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-01-31 23:29:16 +0900

    Title: 공백을 기준으로 출력, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240131/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,11 @@
+str1 = input()
+str2 = input()
+
+for elem in str1:
+    if elem == ' ':
+        continue
+    print(elem, end = '')
+for elem in str2:
+    if elem ==  ' ':
+        continue
+    print(elem, end = '')
