0cb79d08963f41882caf7e322468ba394a3b4437
rudrb1231 <136680921+rudrb1231@users.noreply.github.com>
2024-02-22 22:16:12 +0900

    Title: 함수를 이용한 소수 판별, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240222/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,22 @@
+a,b=map(int,input().split())
+
+cnt=0
+
+
+def is_prime(n):
+    if n==1:
+        return False
+
+
+    for i in range(2, n):
+        if n%i ==0:
+            return False
+    return True
+
+sum_val=0
+
+for i in range(a,b+1):
+    if is_prime(i):
+        sum_val+=i
+
+print(sum_val)
